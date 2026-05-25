5b66b9992b96ecb9dd8ad0c57f2ca13dea4e0112
Michael Gilbertson <mgilbertson@grassrootsanalytics.com>
2026-05-25 10:44:01 -0400

    Update bonsai to 0.1.2

--- Formula/bonsai.rb
+++ Formula/bonsai.rb
@@ -5,7 +5,7 @@ class Bonsai < Formula
 
   desc "Manage per-branch git worktrees with ports and Caddy URLs"
   homepage "https://github.com/mggwxyz/bonsai"
-  url "https://github.com/mggwxyz/bonsai.git", tag: "v0.1.1"
+  url "https://github.com/mggwxyz/bonsai.git", tag: "v0.1.2"
   license "MIT"
 
   depends_on "caddy"
